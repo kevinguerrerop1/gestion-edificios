eed4a34303d0e4b67d6b9185348e0628eec2d7bb
Kevin Guerrero <kevinguerrerop1@gmail.com>
2026-01-19 16:49:01 -0300

    Cambio nombre de modelo

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Reporte;
-use App\Models\Gestiones;
+use App\Models\gestiones;
 use App\Models\Edificio;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
